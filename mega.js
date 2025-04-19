7c5b7d61139b125e3be4d1b109f5999b6383dd2a
SANIJA-MD-OFFICIAL-3 <ggsanija764@gmail.com>
2025-04-19 14:10:01 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,28 +1,32 @@
-const mega = require('megajs');
+const mega = require("megajs");
 
 const auth = {
-  email: process.env.MEGA_EMAIL,
-  password: process.env.MEGA_PASSWORD,
-  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    email: 'indranikarunarathna80@gmail.com',
+    password: 'sanija1234',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
 const upload = (dataBuffer, name) => {
-  return new Promise((resolve, reject) => {
-    const storage = new mega.Storage(auth, () => {
-      const file = storage.upload({ name });
-      file.end(dataBuffer);
-      storage.on('add', file => {
-        file.link((err, url) => {
-          if (err) {
+    return new Promise((resolve, reject) => {
+        try {
+            const storage = new mega.Storage(auth, () => {
+                const file = storage.upload({ name: name });
+                file.end(dataBuffer);
+                storage.on("add", (file) => {
+                    file.link((err, url) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                        storage.close();
+                        resolve(url);
+                    });
+                });
+            });
+        } catch (err) {
             reject(err);
-            return;
-          }
-          storage.close();
-          resolve(url);
-        });
-      });
+        }
     });
-  });
 };
 
 module.exports = { upload };
