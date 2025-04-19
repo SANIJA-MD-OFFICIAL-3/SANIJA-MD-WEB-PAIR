c1616d1780c285f7d308f4e88b4776d6ef356f18
SANIJA-MD-OFFICIAL-3 <ggsanija764@gmail.com>
2025-04-19 13:38:23 +0530

    Create mega.js

---
+++ mega.js
@@ -0,0 +1,31 @@
+const mega = require("megajs");
+
+const auth = {
+    email: 'indranikarunarathna80@gmail.com',
+    password: 'sanija1234',
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+};
+
+const upload = (data, name) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const storage = new mega.Storage(auth, () => {
+                data.pipe(storage.upload({ name: name, allowUploadBuffering: true }));
+                storage.on("add", (file) => {
+                    file.link((err, url) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                        storage.close();
+                        resolve(url);
+                    });
+                });
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
+module.exports = { upload };
