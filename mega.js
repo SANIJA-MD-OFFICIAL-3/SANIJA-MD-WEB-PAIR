c15982b3585069369c3232c446f78cf80ed91234
SANIJA-MD-OFFICIAL-3 <ggsanija764@gmail.com>
2025-04-19 14:15:52 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,18 +1,17 @@
-const mega = require("megajs");
+const mega = require('megajs');
 
 const auth = {
     email: 'indranikarunarathna80@gmail.com',
     password: 'sanija1234',
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
-const upload = (dataBuffer, name) => {
+const upload = (data, name) => {
     return new Promise((resolve, reject) => {
         try {
             const storage = new mega.Storage(auth, () => {
-                const file = storage.upload({ name: name });
-                file.end(dataBuffer);
-                storage.on("add", (file) => {
+                data.pipe(storage.upload({ name: name, allowUploadBuffering: true }));
+                storage.on('add', file => {
                     file.link((err, url) => {
                         if (err) {
                             reject(err);
