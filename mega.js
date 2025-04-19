ee16db62d76d847dd9d9a3d43bdcd8ea1b419b8d
SANIJA-MD-OFFICIAL-3 <ggsanija764@gmail.com>
2025-04-19 13:51:42 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,9 +1,9 @@
 const mega = require('megajs');
 
 const auth = {
-  email: 'indranikarunarathna80@gmail.com',
-  password: 'sanija1234',
-  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+  email: process.env.MEGA_EMAIL,
+  password: process.env.MEGA_PASSWORD,
+  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
 };
 
 const upload = (dataBuffer, name) => {
