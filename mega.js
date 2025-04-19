14a17bfd9875b5a3282fe86842e4157199a7ab6f
SANIJA-MD-OFFICIAL-3 <ggsanija764@gmail.com>
2025-04-19 13:45:56 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,31 +1,28 @@
-const mega = require("megajs");
+const mega = require('megajs');
 
 const auth = {
-    email: 'indranikarunarathna80@gmail.com',
-    password: 'sanija1234',
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+  email: 'indranikarunarathna80@gmail.com',
+  password: 'sanija1234',
+  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
-const upload = (data, name) => {
-    return new Promise((resolve, reject) => {
-        try {
-            const storage = new mega.Storage(auth, () => {
-                data.pipe(storage.upload({ name: name, allowUploadBuffering: true }));
-                storage.on("add", (file) => {
-                    file.link((err, url) => {
-                        if (err) {
-                            reject(err);
-                            return;
-                        }
-                        storage.close();
-                        resolve(url);
-                    });
-                });
-            });
-        } catch (err) {
+const upload = (dataBuffer, name) => {
+  return new Promise((resolve, reject) => {
+    const storage = new mega.Storage(auth, () => {
+      const file = storage.upload({ name });
+      file.end(dataBuffer);
+      storage.on('add', file => {
+        file.link((err, url) => {
+          if (err) {
             reject(err);
-        }
+            return;
+          }
+          storage.close();
+          resolve(url);
+        });
+      });
     });
+  });
 };
 
 module.exports = { upload };
